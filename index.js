bc0a23eb87eec004fa891d65cc485bdb94a42d50
Julian Piñel <julianpiniel@gmail.com>
2021-08-23 20:46:34 -0300

    trying docker & docker-compose

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const app = require("./src/app.js");
+require("./src/models/index");
 
 const server = app.listen(process.env.PORT, () => {
 	console.log(`%s listening at ${process.env.PORT}`); // eslint-disable-line no-console
